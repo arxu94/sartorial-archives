36e098db20ddc959e3a9a098f8a566f4854d5101
Ariel Xu <arielxu@live.com>
2021-02-20 19:08:33 +0800

    updated controllers for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+
+    def token_request
+      token_params = {
+        appId: ENV['WECHAT_APP_ID'],
+        secret: ENV['WECHAT_APP_SECRET'],
+        grant_type: "client_credential"
+      }
+      @wechat_response ||= RestClient.get("https://api.weixin.qq.com/cgi-bin/token", params: token_params)
+      @getToken ||= JSON.parse(@wechat_response.body)
+  end
+
 end
